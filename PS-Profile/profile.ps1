15123a3f0467bd7eefa8f27859b2375b999cf1f7
cotoro <76488848+cotoro-lab@users.noreply.github.com>
2023-10-27 14:35:14 +0900

    Create profile.ps1

---
+++ PS-Profile/profile.ps1
@@ -0,0 +1,18 @@
+# --- Function -------------------------------------
+Function EditVimrc { vim ~/.vimrc }
+Function EditProfile { vim "C:\Program Files\PowerShell\7\profile.ps1" }
+
+function Vifmcd
+{
+    $dst = & vifm --choose-dir - $args
+    if ([string]::IsNullOrEmpty($dst)) {
+        Write-Host 'Directory picking cancelled/failed'
+        return 1
+    }
+    Set-Location -LiteralPath $dst
+}
+
+# --- Alias -------------------------------------"
+Set-Alias -Name ran -Value Vifmcd
+Set-Alias -Name vimrc -Value EditVimrc
+Set-Alias -Name profile -Value EditProfile
