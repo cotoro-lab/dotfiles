50f3335102b68d5de5aca9e573f549f2067fe484
cotoro <76488848+cotoro-lab@users.noreply.github.com>
2024-02-29 10:32:48 +0900

    Update profile.ps1

--- PS-Profile/profile.ps1
+++ PS-Profile/profile.ps1
@@ -5,7 +5,7 @@
 # --- Function -------------------------------------
 Function EditVimrc { nvim ~/.vimrc }
 Function EditProfile { nvim "C:\Program Files\PowerShell\7\profile.ps1" }
-Function CdNvim { Set-Location "C:\Users\s.uto\AppData\Local\nvim" }
+Function CdNeovim { Set-Location "C:\Users\s.uto\AppData\Local\nvim" }
 Function Open-CurrentDirectory {
     Invoke-Item .
 }
@@ -25,7 +25,7 @@ Set-Alias -Name ran -Value Vifmcd
 Set-Alias -Name vimrc -Value EditVimrc
 Set-Alias -Name profile -Value EditProfile
 Set-Alias -Name open -Value Open-CurrentDirectory
-Set-Alias -Name cdnvim -Value CdNvim
+Set-Alias -Name cdnvim -Value CdNeovim
 
 Set-Alias -Name grep -Value Select-String
 Set-Alias -Name clip -Value Set-Clipboard
